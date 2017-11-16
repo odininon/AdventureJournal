ca959ae14dfe7273b32f9d0eeba3f5539253a06b
Odininon <mike.stengel@gmail.com>
2017-11-15 19:29:04 -0500

    Initial Commit

---
+++ AdventuresJournal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdventuresJournal
+//
+//  Created by Michael Stengel on 11/15/17.
+//  Copyright © 2017 Michael Stengel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
